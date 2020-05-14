346ef55160c0ff63d71102a66eb251526ec92c3c
Paulina Szyszko <paulinabuczel@gmail.com>
2020-05-14 18:59:51 +0200

    Use Promises chaining with callbacks

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,13 +1,27 @@
-const doWorkPromis = new Promise((resolve, reject) => {
-    setTimeout(() => {
-       // resolve([7,4,5])
-        reject('Things went wrong!')
+const add = (a, b,) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
 
-    }, 2000)
-})
+// add(1, 2).then((sum) => {
+//     console.log(sum)
+//     add(sum,5).then((sum2) => {
+//         console.log(sum2)
+//     }).catch((e) => {
+//         console.log(sum2)
+//     })
+// }).catch((e) => {
+//     console.log(e)
+// })
 
-doWorkPromis.then((result) => {
-    console.log('Success!', result)
-}).catch((error) => {
-   console.log('Error', error)
+add(1, 1).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log((sum2))
+}).catch((e) => {
+    console.log(e)
 })
