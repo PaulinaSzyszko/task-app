e5ff0ddece6725b68bbc6bc754171219cc9980a5
Paulina Szyszko <paulinabuczel@gmail.com>
2020-05-18 20:21:11 +0200

    login validation for user added

--- src/routers/task.js
+++ src/routers/task.js
@@ -38,15 +38,19 @@ router.get('/tasks/:id', async (req, res) => {
 })
 
 router.patch('/tasks/:id', async (req, res) => {
-    const update = Object.keys(req.body)
+    const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
-    const isValid = update.every((update) => allowedUpdates.includes(update))
+    const isValid = updates.every((update) => allowedUpdates.includes(update))
 
     if (!isValid) {
         return res.status(400).send({error: 'Invalid updates!'})
     }
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        const task = await Task.findById(req.params.id)
+
+        updates.forEach((update) => task[update] = req.body[update])
+         await task.save()
+
         if (!task) {
             return res.status(404).send()
         }
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,15 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -39,7 +48,7 @@ router.get('/users/:id', async (req, res) => {
 
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'age']
+    const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
     if (!isValidOperation) {
@@ -47,7 +56,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
         if (!user) {
             return res.status(404).send()
         }
