ea2722196e42cc4ba6238ba9566d3a7cebb04def
Paulina Szyszko <paulinabuczel@gmail.com>
2020-05-20 21:33:10 +0200

    login token added to application. Authorization when user log in to the application

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Service is temporary unavailable')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -20,7 +32,7 @@ const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
     const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
-    console.log('token:' +token)
+    console.log('token:' + token)
 
     const data = jwt.verify(token, 'thisismynewcourse')
     console.log(data)
